007b72f09a9556b1a85aed36f782a66f42ba68bf
Óliver Silva <n2oliver@github.com>
2022-05-08 16:43:08 -0300

    feat: adiciona a pausa.

--- js/Ball.js
+++ js/Ball.js
@@ -9,62 +9,63 @@ class Ball extends BallBase {
                 paraBaixo: false
             }
             return setInterval(() => {
-                const objectStyle = document.getElementById(attributes.id).style;
-                const limiteHorizontalInferior = window.innerHeight - 120;
-                const supportBarLeft = document.getElementById(attributes.supportBarId).offsetLeft;
-                const supportBarRight = supportBarLeft + parseInt(document.getElementById(attributes.supportBarId).style.width) + 2;
-                const top = parseInt(document.getElementById(attributes.id).offsetTop);
-                const left = parseInt(document.getElementById(attributes.id).offsetLeft);
-                const right = left + attributes.width;
-                const height = parseInt(attributes.height) + 2;
+                
 
-                if(left <= 0) {
-                    estadoDaDirecao.paraAEsquerda = false;
-                    estadoDaDirecao.paraADireita = true;
-                }
-                if(!estadoDaDirecao.paraADireita){
-                    estadoDaDirecao.paraAEsquerda = true;
-                    estadoDaDirecao.paraADireita = false;
-                }
+                if(!window.pause) { 
+                    const objectStyle = document.getElementById(attributes.id).style;
+                    const limiteHorizontalInferior = window.innerHeight - 120;
+                    const supportBarLeft = document.getElementById(attributes.supportBarId).offsetLeft;
+                    const supportBarRight = supportBarLeft + parseInt(document.getElementById(attributes.supportBarId).style.width) + 2;
+                    const top = parseInt(document.getElementById(attributes.id).offsetTop);
+                    const left = parseInt(document.getElementById(attributes.id).offsetLeft);
+                    const right = left + attributes.width;
+                    const height = parseInt(attributes.height) + 2;
 
-                if(top <= 0) {
-                    estadoDaDirecao.paraCima = false;
-                    estadoDaDirecao.paraBaixo = true;
-                }
-                if(!estadoDaDirecao.paraBaixo){
-                    estadoDaDirecao.paraCima = true;
-                    estadoDaDirecao.paraBaixo = false;
-                }
-
-                if(left >= window.innerWidth - 32) {
-                    estadoDaDirecao.paraAEsquerda = true;
-                    estadoDaDirecao.paraADireita = false;
-                }
-                if(!estadoDaDirecao.paraAEsquerda){
-                    estadoDaDirecao.paraAEsquerda = false;
-                    estadoDaDirecao.paraADireita = true;
-                }
-
-                if(top + height >= limiteHorizontalInferior && top <= limiteHorizontalInferior && right >= supportBarLeft - 32 && left <= supportBarRight) {
-                    estadoDaDirecao.paraCima = true;
-                    estadoDaDirecao.paraBaixo = false;
-                    const audio = document.getElementById("toque-linha-amarela");
-                    if(audio.readyState) {
-                        audio.play();
+                    if(left <= 0) {
+                        estadoDaDirecao.paraAEsquerda = false;
+                        estadoDaDirecao.paraADireita = true;
                     }
-                 
-                } 
-                if(!estadoDaDirecao.paraCima){
-                    estadoDaDirecao.paraCima = false;
-                    estadoDaDirecao.paraBaixo = true;
-                }
+                    if(!estadoDaDirecao.paraADireita){
+                        estadoDaDirecao.paraAEsquerda = true;
+                        estadoDaDirecao.paraADireita = false;
+                    }
 
-                let direction = 3;
-                if(estadoDaDirecao.paraAEsquerda) {
-                    direction = -3;
-                }
+                    if(top <= 0) {
+                        estadoDaDirecao.paraCima = false;
+                        estadoDaDirecao.paraBaixo = true;
+                    }
+                    if(!estadoDaDirecao.paraBaixo){
+                        estadoDaDirecao.paraCima = true;
+                        estadoDaDirecao.paraBaixo = false;
+                    }
 
-                if(!window.pause) {   
+                    if(left >= window.innerWidth - 32) {
+                        estadoDaDirecao.paraAEsquerda = true;
+                        estadoDaDirecao.paraADireita = false;
+                    }
+                    if(!estadoDaDirecao.paraAEsquerda){
+                        estadoDaDirecao.paraAEsquerda = false;
+                        estadoDaDirecao.paraADireita = true;
+                    }
+
+                    if(top + height >= limiteHorizontalInferior && top <= limiteHorizontalInferior && right >= supportBarLeft - 32 && left <= supportBarRight) {
+                        estadoDaDirecao.paraCima = true;
+                        estadoDaDirecao.paraBaixo = false;
+                        const audio = document.getElementById("toque-linha-amarela");
+                        if(audio.readyState) {
+                            audio.play();
+                        }
+                    
+                    } 
+                    if(!estadoDaDirecao.paraCima){
+                        estadoDaDirecao.paraCima = false;
+                        estadoDaDirecao.paraBaixo = true;
+                    }
+
+                    let direction = 3;
+                    if(estadoDaDirecao.paraAEsquerda) {
+                        direction = -3;
+                    }  
                     objectStyle.left = (left + direction * attributes.velocity) + "px";
                     
                     if(estadoDaDirecao.paraCima) {
