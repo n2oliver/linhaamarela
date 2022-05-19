45bba551e2c8e0545d56b2167cdb3e34d435643e
Óliver Silva <n2oliver@github.com>
2022-05-19 16:54:22 -0300

    feat: adiciona inimigos.

---
+++ src/js/SpaceInvader.js
@@ -0,0 +1,45 @@
+class SpaceInvader {
+    init = function (level) {
+        for(let i = 100/level; i < 100; i += (100/level)) {
+            let invader = document.createElement('div');
+            invader.classList.add("invader");
+            invader.style.left = i + "%";
+            document.body.append(invader);
+        }
+
+        let left = true;
+        return setInterval(function () {
+            if(!window.pause) {
+                for(let invader of document.getElementsByClassName('invader')) {
+                    if (invader.offsetLeft > window.innerWidth - 64) {
+                        left = false;
+                    }
+                    if (invader.offsetLeft < 0) {
+                        left = true;
+                    }
+                    if (left == true) {
+                        invader.style.left = (invader.offsetLeft + 10) + "px"
+                    } else {
+                        invader.style.left = (invader.offsetLeft - 10) + "px"
+                    }
+                    Array.from(document.body.children).filter((elem) => {
+                        if (elem.nodeType == Node.ELEMENT_NODE && elem.classList.contains("capsule")
+                        && (elem.offsetLeft < invader.offsetLeft + invader.clientWidth) &&
+                        (elem.offsetLeft + elem.clientWidth > invader.offsetLeft) 
+                            &&
+                            (elem.offsetTop < invader.offsetTop + invader.clientHeight) &&
+                        (elem.offsetTop + elem.clientHeight > invader.offsetTop) 
+                    ) {
+                        invader.remove()
+                        }
+                    });
+                }
+            }
+        }, 100);
+    }
+    destroy = function () {
+        for(let invader of Object.assign([], document.getElementsByClassName("invader"))) {
+            invader.remove()
+        }
+    }        
+}
