d3c2ecd007a26d7e4744dd91217fe37ad84fc7b0
Óliver Silva <n2oliver@github.com>
2022-05-21 19:26:43 -0300

    feat: adicionando router.

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
+use GuzzleHttp\Psr7\LazyOpenStream;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$app = AppFactory::create();
+
+$app->get('/', function (Request $request, Response $response, $args) {
+
+    $newStream = new LazyOpenStream('index.html', 'r');
+    $newResponse = $response->withBody($newStream);
+    return $newResponse;
+});
+
+$app->run();
