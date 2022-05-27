ce44ed5cea2e625661a648cc56bc1ef128a0b9c7
Óliver Silva <n2oliver@github.com>
2022-05-27 11:55:29 -0300

    feat: refatora arquitetura de rotas.

--- controllers/UsuarioController.php
+++ controllers/UsuarioController.php
@@ -3,6 +3,9 @@ namespace App\Controllers;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use App\Validators\UsuarioValidator;
+use Db\Eloquent\Models\Usuario;
+use \Firebase\JWT\JWT;
+use \Firebase\JWT\Key;
 
 class UsuarioController {
     public function postInscricao (Request $request, Response $response) {
@@ -16,5 +19,16 @@ class UsuarioController {
         $response->getBody()->write($payload);
         return $response->withHeader('Content-Type', 'application/json')
             ->withStatus(201);
+    }
+    public function postLogin(Request $request, Response $response, $args) {
+        $formData = $request->getParsedBody();
+        $usuarios = Usuario::where('nomedeusuario', '=', $formData['nomedeusuario'])->where('senha', '=', $formData['senha'])->get();
+        
+        $payload = json_encode($usuarios);
+        $response->getBody()->write($payload);
+        echo '<pre>' . JWT::encode(array('nome'=> 'OK', 'senha' => '123456', 'token' => 'asdokasdkpasokdaspodkpaskdpk'), 'minha_chave_secreta', 'HS256'). "<br><pre>";
+        return $response
+                ->withHeader('Content-Type', 'application/json')
+                ->withStatus(201);
     }
 }
