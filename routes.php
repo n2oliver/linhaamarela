cd8e0ef6fe268ec30faa51e9a095cb7e45717109
Óliver Silva <n2oliver@github.com>
2022-05-27 13:38:04 -0300

    feat: separa controller de login do controller de inscricao.

--- routes.php
+++ routes.php
@@ -2,12 +2,13 @@
 namespace App;
 use App\Controllers\HomeController;
 use App\Controllers\UsuarioInscricaoController;
+use App\Controllers\UsuarioLoginController;
 
 class Routes {
     public function run($app) {
         $app->get('/', [HomeController::class, 'inicio']);
         $app->post('/inscricao', [UsuarioInscricaoController::class, 'postInscricao']);
-        $app->post('/login', [UsuarioController::class, 'postLogin']);
+        $app->post('/login', [UsuarioLoginController::class, 'postLogin']);
         return $app;
     }
 }
