a72226b18b5c93e25054b4948b09cce67b2b5702
Óliver Silva <n2oliver@github.com>
2022-05-27 13:20:35 -0300

    feat: refatora arquitetura de rotas.

--- config/config.php
+++
@@ -1,2 +0,0 @@
-<?php
-$config['composer_autoload'] = true;
--- index.php
+++ index.php
@@ -1,21 +1,7 @@
 <?php
 
 require __DIR__ . '/../vendor/autoload.php';
-use Psr\Http\Message\ResponseInterface as Response;
-use Psr\Http\Message\ServerRequestInterface as Request;
-use Slim\Factory\AppFactory;
-use \Illuminate\Database\Capsule\Manager;
-use App\Controllers\HomeController;
-use App\Controllers\UsuarioController;
+use App\App;
 
-$app = AppFactory::create();
-$container = $app->getContainer();
-$capsule = new Manager;
-require __DIR__ . '/config/database.php';
-
-$app->get('/', [HomeController::class, 'inicio']);
-$app->post('/inscricao', [UsuarioController::class, 'postInscricao']);
-/** TODO
- * $app->post('/login', [UsuarioController::class, 'postLogin']);
- * **/
+$app = new App;
 $app->run();
