cc90d25f47a54bef4b096ed51d2805582ac1386c
Óliver Silva <n2oliver@github.com>
2022-05-27 13:29:50 -0300

    feat: renomeia controller de inscrição.

--- src/routes.php
+++ src/routes.php
@@ -1,12 +1,12 @@
 <?php
 namespace App;
 use App\Controllers\HomeController;
-use App\Controllers\UsuarioController;
+use App\Controllers\UsuarioInscricaoController;
 
 class Routes {
     public function run($app) {
         $app->get('/', [HomeController::class, 'inicio']);
-        $app->post('/inscricao', [UsuarioController::class, 'postInscricao']);
+        $app->post('/inscricao', [UsuarioInscricaoController::class, 'postInscricao']);
         $app->post('/login', [UsuarioController::class, 'postLogin']);
         return $app;
     }
