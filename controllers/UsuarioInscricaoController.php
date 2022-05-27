99a35f4ce3031b958c04a43fd6176d6e2bcb17e1
Óliver Silva <n2oliver@github.com>
2022-05-27 15:24:23 -0300

    feat: permite o cadastro do usuário.

--- controllers/UsuarioInscricaoController.php
+++ controllers/UsuarioInscricaoController.php
@@ -3,14 +3,22 @@ namespace App\Controllers;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use App\Validators\UsuarioValidator;
+use App\UseCases\UsuarioInscreverUseCase;
 
 class UsuarioInscricaoController {
+    private $inscreverUsuario;
+    
+    public function __construct(?UsuarioInscreverUseCase $usuarioInscreverUseCase) {
+        $this->inscreverUsuario = $usuarioInscreverUseCase ?? new UsuarioInscreverUseCase();
+    }
     public function postInscricao (Request $request, Response $response) {
         $usuarioValidator = new UsuarioValidator();
         $formData = $request->getParsedBody();
         if($validacao = $usuarioValidator->postValidarInscricao($response, $formData)) {
             return $validacao;
         }
+        
+        $this->inscreverUsuario->execute($formData);
         
         $payload = json_encode(array('message' => 'Sucesso!', 'code' => 200));
         $response->getBody()->write($payload);
