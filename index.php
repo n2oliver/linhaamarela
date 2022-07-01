ceae066f1e6bc2ca1e9871409db9193db726ab70
Óliver Silva <n2oliver@github.com>
2022-07-01 20:29:28 -0300

    Config app .htaccess.

--- index.php
+++ index.php
@@ -1,8 +1,5 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-return "OK";
+error_log(__DIR__ . '/vendor/autoload.php');
 require __DIR__ . '/vendor/autoload.php';
 use App\App;
 
