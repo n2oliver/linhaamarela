3c66ef460c713aa5871b05adf96ee900ec555ef3
Óliver Silva <n2oliver@github.com>
2022-07-02 17:46:13 -0300

    Adiciona .env.

--- app/App.php
+++ app/App.php
@@ -4,6 +4,7 @@ use Slim\Factory\AppFactory;
 use \Illuminate\Database\Capsule\Manager;
 use App\Middleware;
 use App\Routes;
+use Dotenv\Dotenv;
 
 class App {
     private $app;
@@ -12,6 +13,11 @@ class App {
 
         $container = $this->app->getContainer();
         $capsule = new Manager;
+        
+        $dotenv = Dotenv::createImmutable(__DIR__.'/../.');
+        $dotenv->load();
+        
+        $dotenv->required(['DB_HOST', 'DB_NAME', 'DB_USER', 'DB_PASS']);
 
         require __DIR__ . '/config/database.php';
 
