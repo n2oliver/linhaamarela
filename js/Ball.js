07f59a630de04fc048c6810f2e3d065467fe8104
Óliver Silva <n2oliver@github.com>
2022-09-04 15:59:14 -0300

    refactor: refatoração.

--- js/Ball.js
+++ js/Ball.js
@@ -1,7 +1,14 @@
-class Ball extends BallBase {
+class Ball extends GameObject {
     constructor(attributes){
         super(attributes);
-        this.build(attributes);
+        this.build = function(attributes) {
+            const styles = {
+                borderRadius: attributes.width + "px",
+                transform: "translateX(16px)",
+                position: attributes.position,
+            }
+            Object.assign(document.getElementById(attributes.id).style, styles);
+        }
         this.init = function(attributes) {
             let estadoDaDirecao = {
                 paraAEsquerda: false,
@@ -10,65 +17,44 @@ class Ball extends BallBase {
                 paraBaixo: false
             }
             return setInterval(() => {
-                
-
                 if(!window.pause) { 
-                    const objectStyle = document.getElementById(attributes.id).style;
+                    const element = document.getElementById(attributes.id);
+                    const supportBar = document.getElementById(attributes.supportBarId);
+                    const objectStyle = element.style;
                     const limiteHorizontalInferior = window.innerHeight - 60;
-                    const supportBarLeft = document.getElementById(attributes.supportBarId).offsetLeft;
-                    const supportBarRight = supportBarLeft + parseInt(document.getElementById(attributes.supportBarId).style.width) + 2;
-                    const top = parseInt(document.getElementById(attributes.id).offsetTop);
-                    const left = parseInt(document.getElementById(attributes.id).offsetLeft);
+                    const supportBarRight = supportBar.offsetLeft + parseInt(supportBar.style.width) + 2;
+                    const top = parseInt(element.offsetTop);
+                    const left = parseInt(element.offsetLeft);
                     const right = left + attributes.width;
                     const height = parseInt(attributes.height) + 2;
 
-                    if(left <= 0) {
-                        estadoDaDirecao.paraAEsquerda = false;
-                        estadoDaDirecao.paraADireita = true;
-                    }
-                    if(!estadoDaDirecao.paraADireita){
-                        estadoDaDirecao.paraAEsquerda = true;
-                        estadoDaDirecao.paraADireita = false;
-                    }
+                    if(left <= 0) estadoDaDirecao = Ball.moverParaADireita(estadoDaDirecao);
 
-                    if(top <= 0) {
-                        estadoDaDirecao.paraCima = false;
-                        estadoDaDirecao.paraBaixo = true;
-                    }
-                    if(!estadoDaDirecao.paraBaixo){
-                        estadoDaDirecao.paraCima = true;
-                        estadoDaDirecao.paraBaixo = false;
-                    }
+                    if(!estadoDaDirecao.paraADireita) estadoDaDirecao = Ball.moverParaAEsquerda(estadoDaDirecao);
 
-                    if(left >= window.innerWidth - 32) {
-                        estadoDaDirecao.paraAEsquerda = true;
-                        estadoDaDirecao.paraADireita = false;
-                    }
-                    if(!estadoDaDirecao.paraAEsquerda){
-                        estadoDaDirecao.paraAEsquerda = false;
-                        estadoDaDirecao.paraADireita = true;
-                    }
+                    if(top <= 0) estadoDaDirecao = Ball.moverParaBaixo(estadoDaDirecao);
 
-                    if(top + height >= limiteHorizontalInferior && top <= limiteHorizontalInferior && right >= supportBarLeft - 32 && left <= supportBarRight) {
-                        estadoDaDirecao.paraCima = true;
-                        estadoDaDirecao.paraBaixo = false;
+                    if(!estadoDaDirecao.paraBaixo) estadoDaDirecao = Ball.moverParaCima(estadoDaDirecao);
+
+                    if(left >= window.innerWidth - 32) estadoDaDirecao = Ball.moverParaAEsquerda(estadoDaDirecao);
+ 
+                    if(!estadoDaDirecao.paraAEsquerda) estadoDaDirecao = Ball.moverParaADireita(estadoDaDirecao);
+
+                    if(top + height >= limiteHorizontalInferior && top <= limiteHorizontalInferior && right >= supportBar.offsetLeft - 32 && left <= supportBarRight) {
+                        estadoDaDirecao = Ball.moverParaCima(estadoDaDirecao);
                         const audio = document.getElementById("toque-linha-amarela");
-                        if(audio.readyState) {
+                        if(audio && audio.readyState) {
                             audio.play();
                         }
-                    
-                    } 
-                    if(!estadoDaDirecao.paraCima){
-                        estadoDaDirecao.paraCima = false;
-                        estadoDaDirecao.paraBaixo = true;
                     }
 
+                    if(!estadoDaDirecao.paraCima) estadoDaDirecao = Ball.moverParaBaixo(estadoDaDirecao);
+
                     let direction = 3;
-                    if(estadoDaDirecao.paraAEsquerda) {
-                        direction = -3;
-                    }  
+                    if(estadoDaDirecao.paraAEsquerda) direction = -3;
+
                     objectStyle.left = (left + direction * attributes.velocity) + "px";
-                    
+
                     if(estadoDaDirecao.paraCima) {
                         objectStyle.top = (top - 2 * attributes.velocity) + "px";
                     } else if(estadoDaDirecao.paraBaixo) {
@@ -77,5 +63,26 @@ class Ball extends BallBase {
                 }
             }, 50);
         }
+        this.build(attributes);
+    }
+    static moverParaADireita(estadoDaDirecao) {
+        estadoDaDirecao.paraAEsquerda = false;
+        estadoDaDirecao.paraADireita = true;
+        return estadoDaDirecao;
+    }
+    static moverParaAEsquerda(estadoDaDirecao) {
+        estadoDaDirecao.paraAEsquerda = true;
+        estadoDaDirecao.paraADireita = false;
+        return estadoDaDirecao;
+    }
+    static moverParaBaixo(estadoDaDirecao) {
+        estadoDaDirecao.paraCima = false;
+        estadoDaDirecao.paraBaixo = true;
+        return estadoDaDirecao;
+    }
+    static moverParaCima(estadoDaDirecao) {
+        estadoDaDirecao.paraCima = true;
+        estadoDaDirecao.paraBaixo = false;
+        return estadoDaDirecao;
     }
 }
