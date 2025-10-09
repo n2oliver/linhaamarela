10e99f18558b896caa9d5aaf880a5c1b4e60abb3
n2oliver <silva.liver@gmail.com>
2025-10-09 08:25:52 -0300

    remove ads

--- game.php
+++ game.php
@@ -28,8 +28,6 @@ require('./repositories/PontoRepository.php');
         <link rel="stylesheet" href="<?= $APP_URL ?>/css/enemies.css"/>
         <link rel="stylesheet" href="<?= $APP_URL ?>/css/game.css"></style>
         <link rel="stylesheet" href="<?= $APP_URL ?>/css/game-over.css"></style>
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap" rel="stylesheet">
         <link rel="stylesheet" href="/styles-index.css" />
 
--- index.php
+++ index.php
@@ -24,8 +24,6 @@ if(isset($usuario_id)) {
     <link rel="shortcut icon" href="<?= $APP_URL ?>/iconlinhaamarela.ico" type="image/x-icon"/>
     <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/toastify-js/src/toastify.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="/styles-index.css" />
     <link rel="stylesheet" href="/sobre-mim.css" />
