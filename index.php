1b70c6627714fdeeae29cf4d41f16843544f6fec
n2oliver <silva.liver@gmail.com>
2026-01-23 15:50:11 -0300

    melhoria da usabilidade de linhaamarela

--- index.php
+++ index.php
@@ -177,11 +177,6 @@ if ($impressionid) {
     <script async src="https://appsha-pnd.ctengine.io/js/script.js?wkey=97NjKiTr7b"></script>
 
   <script src="/js/anuncios.js"></script>
-  <script>
-    setTimeout(()=>{
-        abrirSmartlinkUmaVez();
-    }, 3200);
-  </script>
 <?php include("../../gtagmanager.php"); ?>
     <div class="spinner d-none"></div>
     <audio id="main-menu-sound" src="<?= $APP_URL ?>/mp3/try-infraction-main-version.mp3" controls style="display: none" preload="auto"></audio>
